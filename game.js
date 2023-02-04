45736cdb5182177a4ebcf7e309316cdd7a18acf9
L0kesh16 <119944020+L0kesh16@users.noreply.github.com>
2023-02-04 10:46:32 +0530

    Initail

---
+++ game.js
@@ -0,0 +1,108 @@
+
+var count=0;
+var color = ["red","blue","green","yellow"];
+var gamepatter=[];
+var st=1;
+
+// A to Start
+
+document.addEventListener("keypress",function start(event)
+{
+   
+    if(event.key=='a' && st ==1)
+    {
+        document.querySelector("h1").innerHTML="Level "+count;
+        count++;
+        st=0;
+        nextsequence();
+        
+    }
+});
+
+// Clicking Animation and Sound
+
+var audio;
+function anime(c)
+{
+    document.getElementById(c).classList.add("pressed");
+    var audio = new Audio("sounds/"+c+".mp3");
+    audio.play();
+    
+    setTimeout(function()
+    {
+        document.getElementById(c).classList.remove("pressed");
+    },100);
+}
+function clickanime(c)
+{
+document.getElementById(c).classList.add("pressed");
+setTimeout(function()
+    {
+    document.getElementById(c).classList.remove("pressed");
+    },100);
+    audio = new Audio("sounds/"+c+".mp3");
+    audio.play();
+}
+
+//Generation of Random Buttons
+
+
+function nextsequence(){
+    rn=Math.random();
+    rn=rn*4;
+    rn=Math.floor(rn); 
+    gamepatter.push(color[rn]);
+    anime(color[rn]);  
+}
+var userarr= [];
+
+//Button Clicking
+
+function userclick(event){
+    
+  
+    var uc =document.getElementById(event.target.id);
+    ucc=uc.id;
+    userarr.push(ucc);
+    clickanime(ucc);
+ 
+    if(userarr.length==count)
+    {
+        gamewin();
+    }
+}
+
+//Checking Whether the User clicks the button on Correct Order or not
+
+function gamewin(){
+    var z=0;
+  
+    for(var i =0;i<count;i++)
+    {
+        if(userarr[i]===gamepatter[i])
+        {
+            z++;
+        }
+        else{
+        userarr= [];
+        document.querySelector("body").classList.add("game-over");
+        setTimeout(function(){ document.querySelector("body").classList.remove("game-over");},100);
+        document.querySelector("h1").innerHTML="you Lost!, Try Again 😕"
+        setTimeout(function(){
+        location.reload();
+        },1500);     
+    }
+    }    
+    if(z==count)
+    {
+        userarr= [];
+        st=1;
+        document.querySelector("h1").innerHTML="You win! and press A to continue" 
+    }  
+}
+        
+    
+
+
+
+
